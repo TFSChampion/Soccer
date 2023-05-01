cdd4316a6f961e0e036baac70ff8304b95283a69
TFSChampion <darcy_sutton28@student.friends.tas.edu.au>
2023-05-01 15:54:11 +1000

    defined move functions

--- classes.py
+++ classes.py
@@ -1,9 +1,14 @@
+from ev3dev2.motor import Motor, OUTPUT_A, OUTPUT_B
+
 class Bot:
 
     def __init__(self, name: str, leader: bool):
         
         self.name = name
         self.leader = bool
+
+        self.motor_1 = Motor(OUTPUT_A)
+        self.motor_2 = Motor(OUTPUT_B)
 
     def test(self):
 
@@ -14,3 +19,11 @@ class Bot:
         else:
 
             print(f"Hello, my name is {self.name} and I am not the leader.")
+
+    def move_distance(self, delta_x, delta_y):
+
+        pass
+
+    def move_theta(self, delta_p, theta):
+
+        pass
