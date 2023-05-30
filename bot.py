bd7138f522520f996ff894d72804531dcf84d9f5
TFSChampion <darcy_sutton28@student.friends.tas.edu.au>
2023-05-30 10:25:13 +1000

    created movement functions and updated get_ball_pos

--- bot.py
+++ bot.py
@@ -1,36 +1,53 @@
 from pybricks.hubs import EV3Brick
 import pybricks.ev3devices as devices
-from pybricks.parameters import Port, Direction
+from pybricks.parameters import Port
 import math
 
 class Bot:
 
-    def __init__(self, name: str, leader: bool):
-        
-        self.name = name
-        self.leader = bool
+    def __init__(self, goalie : bool):
+
+        self.goalie = goalie
 
         self.ir_right = devices.InfraredSensor(Port.S1)
-        self.ir_left = devices.InfraredSensor(Port.S2)
+        self.ir_left = devices.InfraredSensor(Port.S4)
 
-    def move_distance(self, delta_x : float, delta_y : float, speed: int):
+        if goalie:
 
-        theta = math.round(math.degrees(math.atan(delta_y/delta_x)))
+            self.mo_front = devices.Motor(Port.A)
+            self.mo_back = devices.Motor(Port.B)
 
-        delta_p = math.sqrt(delta_x**2 + delta_y**2)
+        self.mo_right = devices.Motor(Port.C)
+        self.mo_left = devices.Motor(Port.D)
 
-        self.move_theta(self, delta_p, theta, speed)
+    def motor_on_theta(self, theta : float, speed : int):
 
-    def move_theta(self, delta_p : float, theta : float, speed : int):
+        if self.goalie:
 
-        pass
+            raise NotImplementedError("motor_on_theta is not implemented when bot is a goalie.")
+
+        vertical_speed_coef = math.cos(theta)
+        horizontal_speed_coef = math.sin(theta)
+
+        self.mo_front.run(int(speed*vertical_speed_coef))
+        self.mo_back.run(int(speed*vertical_speed_coef))
+        self.mo_right.run(int(speed*horizontal_speed_coef))
+        self.mo_left.run(int(speed*horizontal_speed_coef))
+
+    def start_turn(self, clockwise : bool, speed : int):
+
+        self.mo_left.run(speed if clockwise else 0)
+        self.mo_right.run(0 if clockwise else speed)
 
     def get_ball_pos(self):
 
-        d = None
+        d = 100
 
-        r_left = self.ir_left.distance()
-        r_right = self.r_right.distance()
+        relative_distance_left = self.ir_left.distance()
+        relative_distance_right = self.ir_right.distance()
+
+        r_left = (relative_distance_left/100)*600
+        r_right = (relative_distance_right/100)*600
 
         theta = 90 - (math.acos((d**2+r_left**2-r_right**2)/(2*d*r_left))*(180/math.pi))
 
